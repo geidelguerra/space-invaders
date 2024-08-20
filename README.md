4027845d3d623669c7b64800acdd80f982179bd2
Geidel Guerra <github.6870z@passmail.net>
2024-08-20 17:47:56 -0400

    wip

--- README.md
+++ README.md
@@ -2,5 +2,7 @@
 
 The best way to learn is by doing, so this is my attempt to make Space Invaders using C and Raylib
 
+![image](screenshot.png)
+
 ## References
 - https://github.com/raysan5/raylib
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "raylib.h"
+#include "raymath.h"
 
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 800
@@ -9,12 +10,10 @@
 #define PROJECTILE_SPEED 600
 #define PROJECTILE_OFFSET_FROM_PLAYER 10
 #define PROJECTILE_EXPLOSION_DURATION 0.5 // seconds
-#define ENEMIES_COLS 1
-#define ENEMIES_ROWS 1
-#define MAX_NUM_OF_ENEMIES ENEMIES_ROWS * ENEMIES_COLS + 1
-#define SPACE_BETWEEN_ENEMIES 22
-#define ENEMY_HORIZONTAL_SPEED 150
-#define ENEMY_VERTICAL_SPEED 500
+#define ENEMIES_COLS 11
+#define ENEMIES_ROWS 5
+#define MAX_NUM_OF_ENEMIES ENEMIES_ROWS * ENEMIES_COLS
+#define ENEMY_VERTICAL_MAX_DISTANCE 50
 #define ENEMY_DYING_DURATION 0.5
 
 typedef enum EntityStateValue {
@@ -29,11 +28,12 @@ typedef enum EntityStateValue {
     ENEMY_STATE_DEAD,
 } EntityStateValue;
 
-typedef enum MovementDirectionValue {
+typedef enum MoveDirValue {
+    MOVE_NOTSET,
     MOVE_RIGHT,
     MOVE_DOWN,
     MOVE_LEFT,
-} MovementDirectionValue;
+} MoveDirValue;
 
 typedef struct EntityState {
     EntityStateValue value;
@@ -56,28 +56,50 @@ typedef struct Player {
 
 typedef struct Enemy {
     Rectangle body;
+    Vector2 position;
+    Vector2 velocity;
+    Vector2 acceleration;
     EntityState state;
-    MovementDirectionValue dir;
+    MoveDirValue dir;
+    MoveDirValue previousDir;
+    Vector2 moveStartPosition;
+    float distanceTraveled;
 } Enemy;
+
+typedef struct EnemyFlock {
+    Enemy entities[MAX_NUM_OF_ENEMIES];
+    Rectangle boundaries;
+    MoveDirValue moveDirection;
+} EnemyFlock;
 
 typedef struct Game {
     Player player;
-    Enemy enemies[MAX_NUM_OF_ENEMIES];
-    Rectangle map;
+    EnemyFlock enemyFlock;
+    Rectangle boundaries;
     Rectangle playerUIRect;
 } Game;
+
+float maxForce = 0.01;
+float maxHSpeed = 100;
+float maxVSpeed = 100;
+float enemyDistance = 10;
+float flockAwarenessDistance = 200;
 
 void InitGame(Game *game);
 void UpdateGame(Game *game);
 void RenderGame(Game *game);
 void SetEntityState(EntityState *state, int value);
 void UpdateEntityState(EntityState *state);
+void UpdateEnemyDistanceTraveled(Enemy *enemy);
+void UpdateEnemyFlock(Game *game, EnemyFlock *flock);
+void RenderEnemyFlock(Game *game, EnemyFlock *flock);
+void InitFlock(Game *game, Vector2 startPosition);
 
 int main() {
     Game game;
 
     InitGame(&game);
-    SetTargetFPS(60);
+    SetTargetFPS(144);
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Invaders");
 
@@ -97,15 +119,15 @@ void InitGame(Game *game) {
     int screenTopMargin = 10;
     int screenBottomMargin = 50;
 
-    game->map.width = SCREEN_WIDTH - screenLeftMargin - screenRightMargin;
-    game->map.height = SCREEN_HEIGHT - screenTopMargin - screenBottomMargin;
-    game->map.x = screenLeftMargin;
-    game->map.y = screenTopMargin;
+    game->boundaries.x = screenLeftMargin;
+    game->boundaries.y = screenTopMargin;
+    game->boundaries.width = SCREEN_WIDTH - screenLeftMargin - screenRightMargin;
+    game->boundaries.height = SCREEN_HEIGHT - screenTopMargin - screenBottomMargin;
 
     game->player.body.width = 50;
     game->player.body.height = 50;
-    game->player.body.x = game->map.x;
-    game->player.body.y = game->map.height + game->map.y - game->player.body.height;
+    game->player.body.x = game->boundaries.x;
+    game->player.body.y = game->boundaries.y + game->boundaries.height - game->player.body.height;
     game->player.lives = PLAYER_MAX_LIVES;
     game->player.score = 0;
     game->player.state.value = PLAYER_STATE_IDLE;
@@ -119,16 +141,10 @@ void InitGame(Game *game) {
     game->playerUIRect.width = SCREEN_WIDTH - screenLeftMargin - screenRightMargin;
     game->playerUIRect.height = screenBottomMargin;
     game->playerUIRect.x = screenLeftMargin;
-    game->playerUIRect.y = game->map.y + game->map.height;
+    game->playerUIRect.y = game->boundaries.y + game->boundaries.height;
 
-    for (int i = 0; i < MAX_NUM_OF_ENEMIES - 1; i++) {
-        game->enemies[i].body.width = 50;
-        game->enemies[i].body.height = 50;
-        game->enemies[i].body.x = game->map.x;
-        game->enemies[i].body.y = game->map.y;
-        game->enemies[i].dir = MOVE_RIGHT;
-        game->enemies[i].state.value = ENEMY_STATE_ACTIVE;
-    }
+    Vector2 startPosition = {game->boundaries.x, game->boundaries.y};
+    InitFlock(game, startPosition);
 }
 
 void UpdateGame(Game *game) {
@@ -140,6 +156,54 @@ void UpdateGame(Game *game) {
 
     if (IsKeyDown(KEY_RIGHT)) {
         playerPosition.x += PLAYER_SPEED * GetFrameTime();
+    }
+
+    if (IsKeyPressed(KEY_ONE)) {
+        maxForce += 5;
+    }
+
+    if (IsKeyPressed(KEY_TWO)) {
+        maxForce -= 5;
+        if (maxForce < 1) {
+            maxForce = 1;
+        }
+    }
+
+    if (IsKeyPressed(KEY_THREE)) {
+        maxVSpeed += 5;
+    }
+
+    if (IsKeyPressed(KEY_FOUR)) {
+        maxVSpeed -= 5;
+        if (maxVSpeed < 5) {
+            maxVSpeed = 5;
+        }
+    }
+
+    if (IsKeyPressed(KEY_FIVE)) {
+        maxHSpeed += 5;
+    }
+
+    if (IsKeyPressed(KEY_SIX)) {
+        maxHSpeed -= 5;
+        if (maxHSpeed < 5) {
+            maxHSpeed = 5;
+        }
+    }
+
+    if (IsKeyPressed(KEY_SEVEN)) {
+        flockAwarenessDistance += 5;
+    }
+
+    if (IsKeyPressed(KEY_EIGHT)) {
+        flockAwarenessDistance -= 5;
+        if (flockAwarenessDistance < 5) {
+            flockAwarenessDistance = 5;
+        }
+    }
+
+    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+        InitFlock(game, GetMousePosition());
     }
 
     if (playerPosition.x != game->player.body.x || playerPosition.y != game->player.body.y) {
@@ -158,10 +222,10 @@ void UpdateGame(Game *game) {
         }
     }
 
-    if (game->player.body.x < game->map.x) {
-        game->player.body.x = game->map.x;
-    } else if (game->player.body.x + game->player.body.width >= game->map.x + game->map.width) {
-        game->player.body.x = game->map.x + game->map.width - game->player.body.width;
+    if (game->player.body.x < game->boundaries.x) {
+        game->player.body.x = game->boundaries.x;
+    } else if (game->player.body.x + game->player.body.width >= game->boundaries.x + game->boundaries.width) {
+        game->player.body.x = game->boundaries.x + game->boundaries.width - game->player.body.width;
     }
 
     UpdateEntityState(&game->player.state);
@@ -174,18 +238,9 @@ void UpdateGame(Game *game) {
     if (game->player.projectile.state.value == PROJECTILE_STATE_ACTIVE) {
         game->player.projectile.body.y -= PROJECTILE_SPEED * GetFrameTime();
 
-        for (int i = 0; i < MAX_NUM_OF_ENEMIES; i++) {
-            if (game->enemies[i].state.value != ENEMY_STATE_DEAD && game->enemies[i].state.value != ENEMY_STATE_DYING) {
-                if (CheckCollisionRecs(game->player.projectile.body, game->enemies[i].body)) {
-                    SetEntityState(&game->enemies[i].state, ENEMY_STATE_DYING);
-                    SetEntityState(&game->player.projectile.state, PROJECTILE_STATE_EXPLODING);
-                }
-            }
-        }
-
         // Out of bounds
-        if (game->player.projectile.body.y <= game->map.y) {
-            game->player.projectile.body.y = game->map.y;
+        if (game->player.projectile.body.y <= game->boundaries.y) {
+            game->player.projectile.body.y = game->boundaries.y;
             SetEntityState(&game->player.projectile.state, PROJECTILE_STATE_INACTIVE);
         }
     } else if (game->player.projectile.state.value == PROJECTILE_STATE_EXPLODING) {
@@ -194,50 +249,7 @@ void UpdateGame(Game *game) {
         }
     }
 
-    // Enemies
-    for (int i = 0; i < MAX_NUM_OF_ENEMIES; i++) {
-        if (game->enemies[i].state.value != ENEMY_STATE_DEAD) {
-            UpdateEntityState(&game->enemies[i].state);
-        }
-
-        if (game->enemies[i].state.value == ENEMY_STATE_DYING) {
-            if (game->enemies[i].state.elapsedTime >= ENEMY_DYING_DURATION) {
-                SetEntityState(&game->enemies[i].state, ENEMY_STATE_DEAD);
-            } else {
-                // TODO: play dying animation
-            }
-        } else if (game->enemies[i].state.value == ENEMY_STATE_ACTIVE) {
-            if (game->enemies[i].dir == MOVE_RIGHT) {
-                game->enemies[i].body.x += ENEMY_HORIZONTAL_SPEED * GetFrameTime();
-
-                if (game->enemies[i].body.x + game->enemies[i].body.width >= game->map.x + game->map.width) {
-                    game->enemies[i].body.x = game->map.x + game->map.width - game->enemies[i].body.width;
-                    game->enemies[i].dir = MOVE_DOWN;
-                }
-            } else if (game->enemies[i].dir == MOVE_DOWN) {
-                game->enemies[i].body.y += ENEMY_VERTICAL_SPEED * GetFrameTime();
-
-                // This is game over
-                if (game->enemies[i].body.y + game->enemies[i].body.height >= game->map.y + game->map.height) {
-                    SetEntityState(&game->enemies[i].state, ENEMY_STATE_DYING);
-                    // TODO: Implement game over
-                } else {
-                    if (game->enemies[i].body.x <= game->map.x) {
-                        game->enemies[i].dir = MOVE_RIGHT;
-                    } else {
-                        game->enemies[i].dir = MOVE_LEFT;
-                    }
-                }
-            } else if (game->enemies[i].dir == MOVE_LEFT) {
-                game->enemies[i].body.x -= ENEMY_HORIZONTAL_SPEED * GetFrameTime();
-
-                if (game->enemies[i].body.x <= game->map.x) {
-                    game->enemies[i].body.x = game->map.x;
-                    game->enemies[i].dir = MOVE_DOWN;
-                }
-            }
-        }
-    }
+    UpdateEnemyFlock(game, &game->enemyFlock);
 }
 
 void RenderGame(Game *game) {
@@ -245,21 +257,13 @@ void RenderGame(Game *game) {
     ClearBackground(BLACK);
 
     // Map
-    DrawRectangleLines(game->map.x, game->map.y, game->map.width, game->map.height, DARKBROWN);
+    DrawRectangleLines(game->boundaries.x, game->boundaries.y, game->boundaries.width, game->boundaries.height, DARKBROWN);
 
     // Player
     DrawRectangle(game->player.body.x, game->player.body.y, game->player.body.width, game->player.body.height, RED);
 
     // Enemies
-    for (int i = 0; i < MAX_NUM_OF_ENEMIES; i++) {
-        Enemy enemy = game->enemies[i];
-
-        if (enemy.state.value == ENEMY_STATE_DEAD) {
-            continue;
-        }
-
-        DrawRectangle(enemy.body.x, enemy.body.y, enemy.body.width, enemy.body.height, RED);
-    }
+    RenderEnemyFlock(game, &game->enemyFlock);
 
     // Projectiles
     if (game->player.projectile.state.value == PROJECTILE_STATE_ACTIVE || game->player.projectile.state.value == PROJECTILE_STATE_EXPLODING) {
@@ -273,8 +277,8 @@ void RenderGame(Game *game) {
     }
 
     // UI
-    char playerText[14 + PLAYER_MAX_LIVES + PLAYER_MAX_SCORE];
-    sprintf(playerText, "Lives %d Score %d", game->player.lives, game->player.score);
+    char playerText[50 + PLAYER_MAX_LIVES + PLAYER_MAX_SCORE];
+    sprintf(playerText, "Lives %d Score %d Max force %0.1f Speed H%0.1f V%0.1f Flock awareness distance %0.1f", game->player.lives, game->player.score, maxForce, maxHSpeed, maxVSpeed, flockAwarenessDistance);
     DrawText(playerText, game->playerUIRect.x, game->playerUIRect.y + 10, 20, YELLOW);
 
     EndDrawing();
@@ -289,4 +293,113 @@ void SetEntityState(EntityState *state, int value) {
 
 void UpdateEntityState(EntityState *state) {
     state->elapsedTime = GetTime() - state->startTime;
+}
+
+void UpdateEnemyDistanceTraveled(Enemy *enemy) {
+    enemy->distanceTraveled = Vector2Distance(enemy->moveStartPosition, enemy->position);
+}
+
+void UpdateEnemyFlock(Game *game, EnemyFlock *enemyFlock) {
+    for (int i = 0; i < MAX_NUM_OF_ENEMIES; i++) {
+        Enemy *entity = &enemyFlock->entities[i];
+        UpdateEntityState(&entity->state);
+
+        if (entity->state.value == ENEMY_STATE_ACTIVE) {
+            entity->distanceTraveled = Vector2Distance(entity->moveStartPosition, entity->position);
+
+            if (entity->dir == MOVE_RIGHT) {
+                entity->position.x += maxHSpeed * GetFrameTime();
+
+                if (entity->body.x + entity->body.width >= game->boundaries.x + game->boundaries.width - 10) {
+                    entity->previousDir = entity->dir;
+                    entity->dir = MOVE_DOWN;
+                    entity->moveStartPosition.x = entity->position.x;
+                    entity->moveStartPosition.y = entity->position.y;
+                }
+            } else if (entity->dir == MOVE_LEFT) {
+                entity->position.x -= maxHSpeed * GetFrameTime();
+
+                if (entity->body.x <= game->boundaries.x + 10) {
+                    entity->previousDir = entity->dir;
+                    entity->dir = MOVE_DOWN;
+                    entity->moveStartPosition.x = entity->position.x;
+                    entity->moveStartPosition.y = entity->position.y;
+                }
+            } else if (entity->dir == MOVE_DOWN) {
+                entity->position.y += maxVSpeed * GetFrameTime();
+
+                if (entity->distanceTraveled >= entity->body.height + enemyDistance) {
+                    if (entity->previousDir == MOVE_LEFT) {
+                        entity->dir = MOVE_RIGHT;
+                    } else if (entity->previousDir == MOVE_RIGHT) {
+                        entity->dir = MOVE_LEFT;
+                    }
+                }
+            }
+
+            entity->body.x = entity->position.x - entity->body.width / 2;
+            entity->body.y = entity->position.y - entity->body.height / 2;
+
+            if (game->player.projectile.state.value == PROJECTILE_STATE_ACTIVE) {
+                if (CheckCollisionRecs(game->player.projectile.body, entity->body)) {
+                    SetEntityState(&entity->state, ENEMY_STATE_DYING);
+                    SetEntityState(&game->player.projectile.state, PROJECTILE_STATE_EXPLODING);
+                    game->player.score += 10;
+                }
+            }
+        } else if (entity->state.value == ENEMY_STATE_DYING) {
+            if (entity->state.elapsedTime >= ENEMY_DYING_DURATION) {
+                SetEntityState(&entity->state, ENEMY_STATE_DEAD);
+            }
+        }
+
+        entity->acceleration.x = 0;
+        entity->acceleration.y = 0;
+    }
+}
+
+void RenderEnemyFlock(Game *game, EnemyFlock *enemyFlock) {
+    for (int i = MAX_NUM_OF_ENEMIES - 1; i >= 0; i--) {
+        Enemy *entity = &enemyFlock->entities[i];
+        if (entity->state.value != ENEMY_STATE_DEAD) {
+            Color color = RED;
+
+            if (i / ENEMIES_COLS == 1) {
+                color = GREEN;
+            } else if (i / ENEMIES_COLS > 1 && i / ENEMIES_COLS < 3) {
+                color = YELLOW;
+            }
+
+            DrawRectangle(
+                entity->body.x,
+                entity->body.y,
+                entity->body.width,
+                entity->body.height,
+                color
+            );
+
+            DrawCircleV(entity->position, 5, YELLOW);
+
+            // DrawLineV(entity->moveStartPosition, entity->position, WHITE);
+        }
+    }
+
+    // DrawRectangleLines(enemyFlock->boundaries.x, enemyFlock->boundaries.y, enemyFlock->boundaries.width, enemyFlock->boundaries.height, YELLOW);
+}
+
+void InitFlock(Game *game, Vector2 startPosition) {
+    float flockWidth = game->enemyFlock.entities[0].body.width / 2 + game->enemyFlock.entities[0].body.width * ENEMIES_COLS + enemyDistance * ENEMIES_COLS;
+    // float flockHeight = game->enemyFlock.entities[0].body.width / 2 + game->enemyFlock.entities[0].body.width * ENEMIES_ROWS + enemyDistance * ENEMIES_ROWS;
+
+    for (int i = 0; i < MAX_NUM_OF_ENEMIES; i++) {
+        Enemy *entity = &game->enemyFlock.entities[i];
+        entity->body.width = 50;
+        entity->body.height = 50;
+        entity->position.x = startPosition.x + (flockWidth  / 2) + enemyDistance + entity->body.width / 2 + entity->body.width * (i % ENEMIES_COLS) + enemyDistance * (i % ENEMIES_COLS);
+        entity->position.y = startPosition.y + enemyDistance + entity->body.height / 2 + entity->body.height  * (i / ENEMIES_COLS) + enemyDistance * (i / ENEMIES_COLS);
+        entity->body.x = startPosition.x - entity->body.width / 2;
+        entity->body.y = startPosition.y - entity->body.height / 2;
+        entity->state.value = ENEMY_STATE_ACTIVE;
+        entity->dir = MOVE_RIGHT;
+    }
 }
